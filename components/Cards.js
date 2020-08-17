730327b3b824e21b7d3bf671eaff3bb25f6cd970
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-08-16 21:31:21 -0400

    updated Cards.js file

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,50 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+function createCard(obj){
+
+    //create elements
+    const card = document.createElement('div');
+    const headlineCont = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
+
+    // adding classes 
+    card.classList.add('card');
+    headlineCont.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+    // adding Content
+    headlineCont.textContent = obj.headline;
+    img.src = obj.authorPhoto;
+    name.textContent = 'By' + obj.authorName;
+
+    //appending to DOM 
+    card.appendChild(headlineCont);
+    card.appendChild(author);
+    author.appendChild(imgCont);
+    imgCont.appendChild(img);
+    author.appendChild(name);
+
+    return card;
+
+}
+
+axios.get(' https://lambda-times-api.herokuapp.com/articles')
+     .then(res =>{
+         const cardsContainer = document.querySelector('.cards-container');
+        const arrayValues = Object.values(res.data.articles);
+        arrayValues.forEach(e => {
+            e.forEach(article => {
+                cardsContainer.appendChild(createCard(article));
+            })
+        })
+     })   
+     .catch(err =>{
+         console.log('This is an Error:', err);
+     });
